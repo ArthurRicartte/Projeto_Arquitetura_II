a08e94076d6f3608f143ee66c5f91f91a954660c
Arthur Ricartte <a.ricartte@gmail.com>
2026-07-03 14:48:09 -0300

    Padronizando o código
    Resolvi padronizar as funções para ficarem alinhadas com as outras etapas

--- Etapa_3/Etapa_3.py
+++ Etapa_3/Etapa_3.py
@@ -1,60 +1,98 @@
 import copy
 
-# Funções úteis:
+#Funções úteis:
 def junta_bits(lista_bits):
+    # Transforma cada inteiro da lista em string e junta todos sem espaços
     return "".join(map(str, lista_bits))
 
+#Função auxiliar para pegar os 4 bits mais a direita e retornar o o nome do registrador para impressão:
 def get_reg(bits):
-    mapeamento_b = {
-        "0000": "mdr",
-        "0001": "pc",
-        "0010": "mbr",
-        "0011": "mbru",
-        "0100": "sp",
-        "0101": "lv",
-        "0110": "cpp",
-        "0111": "opc",
-        "1000": "tos"
-    }
-    bits_b_formatado = "".join(map(str, bits))
-    return mapeamento_b.get(bits_b_formatado, "nenhum")
+                
+                #Dicionário para mapear os 4 bits para um registrador
+                mapeamento_b = {
+                                "0000": "mdr",
+                                "0001": "pc",
+                                "0010": "mbr",
+                                "0011": "mbru",
+                                "0100": "sp",
+                                "0101": "lv",
+                                "0110": "cpp",
+                                "0111": "opc",  
+                                "1000": "tos"   
+                            }
+                
+                #Formatando a lista de bits recebida para uma string 
+                bits_b_formatado = "".join(map(str, bits))
 
-# Operadores lógicos da Mic-1
+                #Tentando achar registrador corrpespondente:
+                return mapeamento_b.get(bits_b_formatado, "nenhum")
+    
+#Definindo operadores lógicos da mic-1:
+
 def andLogic(sup, low):
-    return 0 if sup == 0 else low
+    if sup == 0:
+        return 0
+    else:
+        return low
 
 def orLogic(sup, low):
-    return 1 if sup == 1 else low
+    if sup == 1:
+        return 1
+    else:
+        return low
 
 def notLogic(sig):
-    return 1 if sig == 0 else 0
+    if sig == 0:
+        return 1
+    else:
+        return 0
 
 def xorLogic(sup, low):
-    return 1 if sup != low else 0
+    if sup != low:
+        return 1
+    else:
+        return 0
 
+#Função que verifica os bits da saída da ULA para determinar o campo Z:
 def define_Z(saida_ULA):
-    for bit in saida_ULA:
-        if bit == 1:
+    #Para z ser 1, todos os 32 bits precisam ser 0
+    for i in range(0,32):
+        if (saida_ULA[i] == 1):
             return 0
+    
     return 1
 
+#Função que verifica os bits da saída da ULA para determinar o campo N:
 def define_N(f0, f1, co):
-    if f0 == 1 and f1 == 1:
+    #Caso f0 e f1 indiquem uma operação de soma, N é igual ao co, caso contrário, é igual a 0
+    if (f0 == 1 and f1 == 1):
         return co
+
     return 0
 
-def desloca_esquerda_logico(soma):
-    saida = [0] * 32
-    for i in range(24):
-        saida[i] = soma[i + 8]
-    return saida
+#Função que calcula descolamento à esquerda de 8 bits:
+def desloca_esquerda_logico (soma):
+    saida_deslocada = [0] * 32
 
-def desloca_direita_aritmetico(soma):
-    saida = [0] * 32
-    saida[0] = soma[0]
+    #Deslocando a saída 8 bits à esquerda:
+    for i in range(0, 24):
+        saida_deslocada[i] = soma[i + 8]
+
+    #Retornando saída com deslocamento de 8 bits:
+    return saida_deslocada
+
+#Função que calcula deslocamento aritmético de 1 bit à direita:
+def desloca_direita_aritmetico (soma):
+    saida_deslocada = [0] * 32
+
+    #Duplicando primeiro bit da sequência (por ser uma soma aritmética, duplicamos o primeiro bit)
+    saida_deslocada[0] = soma[0]
     for i in range(1, 32):
-        saida[i] = soma[i - 1]
-    return saida
+        #realizando deslocamento:
+        saida_deslocada[i] = soma[i - 1] 
+
+    #Retornando saída da ULA deslocada 1 bit à direita:
+    return saida_deslocada
 
 def ULA(A, B, f0, f1, enA, enB, invA, inc):
     # entrada superior
@@ -92,48 +130,106 @@ def ULA(A, B, f0, f1, enA, enB, invA, inc):
 
     return or2, or3
 
+#Uma função auxiliar da ula que recebe A e B com 32 bits:
 def aux_ULA(lista_A, lista_B, sll8, sra1, f0, f1, enA, enB, invA, inc):
     saida_final = [0] * 32
-    carry_atual = inc
+    
+    #O sinal inc (0 ou 1) entra como o primeiro carry na posição mais à direita
+    carry_atual = inc  
+
     for i in range(31, -1, -1):
+        #Pega o bit individual atual de cada vetor
         bit_A = lista_A[i]
         bit_B = lista_B[i]
+        
+        #Faz cada bit de a e b entrar na lógica da ULA
         resultado_vai_um, resultado_saida = ULA(bit_A, bit_B, f0, f1, enA, enB, invA, carry_atual)
+        
+        #Guarda o bit gerado na posição correspondente da saída
         saida_final[i] = resultado_saida
+        
+        #Caso a IR determine uma soma de bits, precisamos passar o vai-um de uma iteração para a próxima iteração
         if f0 == 1 and f1 == 1:
             carry_atual = resultado_vai_um
         else:
             carry_atual = 0
 
+    #Ao final das 32 iterações, o último carry_atual gerado na posição 0 é o Carry-out (co)
     co_final = carry_atual
 
-    if sll8 == 1:
+    #Verifca os campos sll8, sra1:
+    saida_deslocada = [0] * 32
+    if (sll8 == 1):
         saida_deslocada = desloca_esquerda_logico(saida_final)
-    elif sra1 == 1:
+    elif(sra1 == 1):
         saida_deslocada = desloca_direita_aritmetico(saida_final)
     else:
         saida_deslocada = saida_final
+    
+    #Determinando N e Z:
 
+    #Para Z, basta analisar se existe algum bit da saída que seja 1:
     z = define_Z(saida_final)
+
+    #Para N, basta analisar f0, f1 e co_final:
     n = define_N(f0, f1, co_final)
+    
     return saida_final, saida_deslocada, n, z, co_final
 
+#Função para pegar entradas no formato de strings e transformar em uma lista de inteiros
 def inicializa_entrada(entrada):
-    return [int(c) for c in entrada]
+    #Pega o tamnho da string pra usar como base na lista
+    tamanho = len(entrada)
 
+
+    entrada_inicializada = [0] * tamanho
+    
+    #Preenche a lista com os respectivos números
+    for i in range(0,tamanho):
+        if entrada[i] == '0':
+            entrada_inicializada[i] = 0
+        elif entrada[i] == '1':
+            entrada_inicializada[i] = 1
+        
+    return entrada_inicializada
+
+#Função para carregar os valores dos registradores no nosso programa:
 def inicializar_registradores(caminho):
+    #Criando dicionário para armazenar registradores:
     registradores = {
-        "mar": [0]*32, "mdr": [0]*32, "pc": [0]*32, "mbr": [0]*8,
-        "sp": [0]*32, "lv": [0]*32, "cpp": [0]*32, "tos": [0]*32,
-        "opc": [0]*32, "h": [0]*32
+        "mar": [0]*32,
+        "mdr": [0]*32,
+        "pc":  [0]*32,
+        "mbr": [0]*8,
+        "sp":  [0]*32,
+        "lv":  [0]*32,
+        "cpp": [0]*32,
+        "tos": [0]*32,
+        "opc": [0]*32,
+        "h":   [0]*32
     }
-    with open(caminho, 'r', encoding='utf-8') as file:
+
+    with open(caminho, 'r', encoding= 'utf-8') as file:
         for linha in file:
+            #Tirando espaços em branco
             linha = linha.strip()
+
+            #Pulando linhas em branco:
             if not linha:
                 continue
+            
+            #Separando registrador e valor em duas variáveis:
             r, valor = linha.split(' = ')
-            registradores[r.lower()] = inicializa_entrada(valor)
+
+            #Convertendo valor em uma lista para guardar no dicionário:
+            valor_listado = inicializa_entrada(valor)
+
+            #Tratando o nome de r para não dar erro:
+            r = r.lower()
+
+            #Inserindo valor do registrador no dicionário:
+            registradores[r] = valor_listado
+    
     return registradores
 
 def ativa_registrador_b(reg, bits):
@@ -160,9 +256,12 @@ def ativa_registrador_b(reg, bits):
     else:
         return [0] * 32
 
+#Função para escrever o valor de sd nos registradores em específico:
 def mapeamento_barramento_c(regs, barramento_c, sd):
     modificados = []
-    # Ordem: H, OPC, TOS, CPP, LV, SP, PC, MDR, MAR
+
+    # Mapeamento direto seguindo a convenção padrão Mic-1 (esquerda para a direita)
+    #Caso cada bit esteja alto (1), signifca que o seu respectivo registrador será modificado 
     if barramento_c[0] == 1:
         regs["h"] = sd
         modificados.append("H")
@@ -190,6 +289,7 @@ def mapeamento_barramento_c(regs, barramento_c, sd):
     if barramento_c[8] == 1:
         regs["mar"] = sd
         modificados.append("MAR")
+
     return modificados
 
 def carregar_memoria_dados(caminho):
@@ -222,20 +322,20 @@ def main():
         # Cabeçalho: estado inicial da memória
         log.write("=" * 60 + "\n")
         log.write("Initial memory state\n")
-        log.write("*" * 30 + "\n")
+        log.write("==*" * 20 + "\n")
         for linha in memoria_dados:
             log.write(linha + "\n")
-        log.write("*" * 30 + "\n")
+        log.write("==*" * 20 + "\n")
 
         # Estado inicial dos registradores
         log.write("Initial register state\n")
-        log.write("*" * 30 + "\n")
+        log.write("==*" * 20 + "\n")
         ordem_regs = ["mar", "mdr", "pc", "mbr", "sp", "lv", "cpp", "tos", "opc", "h"]
         for nome in ordem_regs:
             log.write(f"{nome} = {junta_bits(registradores[nome])}\n")
-        log.write("=" * 60 + "\n")
+        log.write("\n" + "=" * 60 + "\n")
         log.write("Start of Program\n")
-        log.write("=" * 60 + "\n")
+        log.write("=" * 60 + "\n\n")
 
         pc = 1
         for instrucao_str in instrucoes:
@@ -304,25 +404,26 @@ def main():
             log.write(f"c = {' '.join(c_nomes)}\n")
 
             log.write("\n> Registers before instruction\n")
-            log.write("*" * 30 + "\n")
+            log.write("==*" * 20 + "\n")
             for nome in ordem_regs:
                 log.write(f"{nome} = {junta_bits(regs_antes[nome])}\n")
 
             log.write("\n> Registers after instruction\n")
-            log.write("*" * 30 + "\n")
+            log.write("==*" * 20 + "\n")
             for nome in ordem_regs:
                 log.write(f"{nome} = {junta_bits(registradores[nome])}\n")
-
+            
             log.write("\n> Memory after instruction\n")
-            log.write("*" * 30 + "\n")
+            log.write("==*" * 20 + "\n")
             for linha in memoria_dados:
                 log.write(linha + "\n")
-            log.write("=" * 60 + "\n")
+            log.write("==*" * 20 + "\n")
 
             pc += 1
 
         # Exibe se não tiver nada a ser lido
         log.write(f"Cycle {pc}\n")
         log.write("No more lines, EOP.\n")
+        log.write("=" * 60 + "\n")
 
 main()
